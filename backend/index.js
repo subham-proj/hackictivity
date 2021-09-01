554aa9f4efa2910543da5da406d6fc59a04a41c3
subham-proj <3133ss32@gmail.com>
2021-09-01 13:50:46 +0530

    deploy bug fix

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,19 @@ app.use(express.json());
 // routes
 app.use("/api/users", userRoutes);
 app.use("/api/post", postRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
+
 app.use(notFound);
 app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
